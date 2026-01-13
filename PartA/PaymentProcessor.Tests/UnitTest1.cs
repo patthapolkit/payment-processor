16f36d1300ad8ed2ea8a47766d974cc62404590c
Patthapol Kittikun <patthapol.k@gmail.com>
2026-01-13 21:33:24 +0700

    Init project

---
+++ PartA/PaymentProcessor.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentProcessor.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
